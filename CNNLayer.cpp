6cb4271a4c32b5689517ad0bc2103d4de8f87b6d
kushagra <1kushagra1@gmail.com>
2022-10-16 14:21:55 +0530

    fourth

--- CNNLayer.cpp
+++ CNNLayer.cpp
@@ -9,6 +9,11 @@ using filter = array<array<double, N>, N>;
 template<int N, int C>
 using image = array<filter<N>, C>;
 
+template<typename T, typename U>
+T min (const T& x, const U& y) { return std::min(x, static_cast<T>(y)); }
+template<typename T, typename U>
+T max (const T& x, const U& y) { return std::max(x, static_cast<T>(y)); }
+
 template<int N, int channels>
 auto copy_to_vector (const image<N, channels>& X, vector<vector<vector<double>>>& V) {
 	V = vector(channels, vector(N, vector(N, double())));
@@ -49,11 +54,11 @@ struct ConvolutionalLayer {
 
 		for (int f = 0; f < out_channels; f++)
 			for (int i = 0; i < M; i++)
-			for (int j = 0; j < M; j++)
-				for (int x = max(0, P - i); x < min(M, N - i + P); x++)
-					for (int y = max(0, P - j); y < min(M, N - j + P); y++)
-						for (int z = 0; z < in_channels; z++)
-							Y[f][i][j] += X[z][i + x - P][j + y - P] * W[f][z][x][y];
+				for (int j = 0; j < M; j++)
+					for (int x = max(0, P - i); x < min(M, N - i + P); x++)
+						for (int y = max(0, P - j); y < min(M, N - j + P); y++)
+							for (int z = 0; z < in_channels; z++)
+								Y[f][i][j] += X[z][i + x - P][j + y - P] * W[f][z][x][y];
 
 		return std::move(Y);
 	}
@@ -64,9 +69,9 @@ struct ConvolutionalLayer {
 		return evaluate(X);
 	}
 
-	template<int N, int M>
+	template<uint64_t M>
 	auto back_propagate (const image<M, out_channels>& grad_Y) {
-		assert(M == N - K + 1);
+		static constexpr int N = M + K - 1;
 
 		array<image<K, in_channels>, out_channels> grad_W{};
 
@@ -99,21 +104,24 @@ struct ConvolutionalLayer {
 
 };
 
-template<uint64_t K, int P = 0, int stride = 1>
+template<uint64_t K, int P = 0>
 struct MaxPool {
-	static_assert(stride > 0 and P >= 0 and K > P);
+	static_assert(P >= 0 and K > P);
 
 	vector<vector<vector<double>>> last_X;
 
 	template<uint64_t N, uint64_t channels>
 	auto evaluate (const image<N, channels>& X) {
-		static constexpr int M = (N + P * 2) / stride;
+		static constexpr int M = N + P * 2 - K + 1;
 		image<M, channels> Y{};
 
 		for (int c = 0; c < channels; c++)
-			for (int i = -P; i < N + P; i += stride)
-				for (int j = -P; j < N + P; j += stride) {
-					auto &v = Y[c][(i + P) / stride][(j + P) / stride] = std::numeric_limits<double>::min();
+			for (int i = -P; i < N + P; i++)
+				for (int j = -P; j < N + P; j++) {
+					auto &v
+						= Y[c][i + P][j + P]
+							= std::numeric_limits<double>::min();
+
 					for (int x = max(0, -i); x < min(K, N - i); x++)
 						for (int y = max(0, -j); y < min(K, N - j); y++)
 							v = max(v, X[c][i + x][j + y]);
@@ -122,21 +130,21 @@ struct MaxPool {
 		return std::move(Y);
 	}
 
-	template<int N, int channels>
+	template<uint64_t N, uint64_t channels>
 	auto train (const image<N, channels>& X) {
 		copy_to_vector(X, last_X);
 		return evaluate(X);
 	}
 
-	template<uint64_t N, int M, int channels>
+	template<uint64_t M, uint64_t channels>
 	auto back_propagate (const image<M, channels>& grad_Y) {
-		static_assert(M == (N + P * 2) / stride);
+		static constexpr int N = M + K - 1 - P * 2;
 
 		image<N, channels> grad_X{};
 
 		for (int c = 0; c < channels; c++)
-			for (int i = -P; i < N + P; i += stride)
-				for (int j = -P; j < N + P; j += stride) {
+			for (int i = -P; i < N + P; i++)
+				for (int j = -P; j < N + P; j++) {
 					auto max_value = std::numeric_limits<double>::min();
 					for (int x = max(0, -i); x < min(K, N - i); x++)
 						for (int y = max(0, -j); y < min(K, N - j); y++)
@@ -145,7 +153,7 @@ struct MaxPool {
 					for (int x = max(0, -i); x < min(K, N - i); x++)
 						for (int y = max(0, -j); y < min(K, N - j); y++)
 							if (last_X[c][i + x][j + y] == max_value)
-								grad_X[c][i + x][j + y] += grad_Y[c][(i + P) / stride][(j + P) / stride];
+								grad_X[c][i + x][j + y] += grad_Y[c][(i + P)][(j + P)];
 				}
 
 		return std::move(grad_X);
@@ -175,7 +183,7 @@ struct ReLu {
 		return evaluate(std::move(X));
 	}
 
-	template<int N, int channels>
+	template<uint64_t N, uint64_t channels>
 	auto back_propagate (const image<N, channels>& grad_Y) {
 		image<N, channels> grad_X{};
 
@@ -190,7 +198,6 @@ struct ReLu {
 
 };
 
-template<int percent = 50>
 struct DropOut {
 	mt19937 rng;
 	static constexpr uint64_t R = std::numeric_limits<uint32_t>::max();
@@ -200,13 +207,13 @@ struct DropOut {
 	vector<vector<vector<bool>>> memo;
 
 	template<uint64_t N, uint64_t channels>
-	auto evaluate (image<N, channels> X) {
+	auto evaluate (image<N, channels> X, const double p) {
 		memo = vector(channels, vector(N, vector(N, false)));
 
 		for (int f = 0; f < channels; f++)
 			for (int i = 0; i < N; i++)
 				for (int j = 0; j < N; j++)
-					if (rng() < (R * percent / 100))
+					if (rng() < R * p)
 						memo[f][i][j] = true, X[f][i][j] = 0;
 
 		return std::move(X);
@@ -225,34 +232,40 @@ struct DropOut {
 	}
 };
 
-template<int N, int M>
+template<int N, int in_channels, int M, int out_channels>
 struct FullyConnectedLayer {
-	array<array<double, N>, M> W;
+	array<array<array<image<N, in_channels>, M>, M>, out_channels> W;
 
-	vector<double> last_X;
+	vector<vector<vector<double>>> last_X;
 
-	auto evaluate (const array<double, N>& X) {
-		array<double, M> Y{};
-		for (int i = 0; i < M; i++)
-			for (int j = 0; j < N; j++)
-				Y[i] += W[i][j] * X[j];
+	auto evaluate (const image<N, in_channels>& X) {
+		image<M, out_channels> Y{};
+		for (int f = 0; f < out_channels; f++)
+			for (int i = 0; i < M; i++)
+				for (int j = 0; j < M; j++)
+					for (int x = 0; x < in_channels; x++)
+						for (int y = 0; y < N; y++)
+							for (int z = 0; z < N; z++)
+								Y[f][i][j] += W[f][i][j][x][y][z] * X[x][y][z];
 		return std::move(Y);
 	}
 
-	auto train (const array<double, N>& X) {
-		last_X = vector(N, double());
-		for (int i = 0; i < N; i++)
-			last_X[i] = X[i];
+	auto train (const image<N, in_channels>& X) {
+		copy_to_vector(X, last_X);
 		return evaluate(X);
 	}
 
-	auto back_propagate (const array<double, M>& grad_Y) {
-		array<double, N> grad_X{};
+	auto back_propagate (const image<M, out_channels>& grad_Y) {
+		image<N, in_channels> grad_X{};
 
-		for (int i = 0; i < M; i++)
-			for (int j = 0; j < N; j++)
-				grad_X[j] += W[i][j] * grad_Y[i],
-				W[i][j] += -eta * grad_Y[i] * last_X[j];
+		for (int f = 0; f < out_channels; f++)
+			for (int i = 0; i < M; i++)
+				for (int j = 0; j < M; j++)
+					for (int x = 0; x < in_channels; x++)
+						for (int y = 0; y < N; y++)
+							for (int z = 0; z < N; z++)
+								grad_X[x][y][z] += W[f][i][j][x][y][z] * grad_Y[f][i][j],
+								W[f][i][j][x][y][z] += -eta * grad_Y[f][i][j] * last_X[x][y][z];
 
 		return std::move(grad_X);
 	}
@@ -263,19 +276,50 @@ struct model {
 	ConvolutionalLayer<32, 32, 5, 1> conv2;
 	ConvolutionalLayer<32, 64, 3, 1> conv3;
 	ConvolutionalLayer<64, 64, 3, 1> conv4;
-	FullyConnectedLayer<64 * 11 * 11, 2048> fcon1;
-	FullyConnectedLayer<2048, 10> fcon2;
+	FullyConnectedLayer<22, 64, 32, 2> fcon1;
+	FullyConnectedLayer<32, 2, 1, 10> fcon2;
 
-	MaxPool<2, 0, 1> pool1;
-	MaxPool<2, 0, 2> pool2;
+	MaxPool<2, 0> pool1;
+	MaxPool<2, 0> pool2;
 	ReLu relu;
-	DropOut<50> drop;
+	DropOut drop;
 
 	auto evaluate (const image<28, 1>& img) {
-		auto Y = drop.evaluate(pool2.evaluate(relu.evaluate(conv4.evaluate(conv3.evaluate(drop.evaluate(pool1.evaluate(relu.evaluate(conv2.evaluate(conv1.evaluate(img))))))))));
+		return
+			fcon2.evaluate(
+			relu.evaluate(
+			fcon1.evaluate(
+			pool2.evaluate(
+		  	relu.evaluate(
+		  	conv4.evaluate(
+		  	conv3.evaluate(
+			pool1.evaluate(
+			relu.evaluate(
+			conv2.evaluate(
+			conv1.evaluate(img
+				)))))))))));
+	}
+
+	auto back_propagate (const image<1, 10>& grad_Y) {
+		conv1.back_propagate(
+		conv2.back_propagate(
+		relu.back_propagate(
+		pool1.back_propagate(
+		conv3.back_propagate(
+		conv4.back_propagate(
+		relu.back_propagate(
+		pool2.back_propagate(
+		fcon1.back_propagate(
+		relu.back_propagate(
+		fcon2.back_propagate(
+			grad_Y)))))))))));
+	}
+
+	auto train (const pair<vector<image<28, 1>>, int>& T) {
 		
 	}
 };
+
 
 int main(){
     ios_base::sync_with_stdio(0), cin.tie(0);
